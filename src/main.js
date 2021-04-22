72172af055edee82f4a4c7c677b14b13962d4f81
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-04-22 10:09:59 -0400

    Projeto inicial

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import vueresource from "vue-resource";
+
+Vue.use(vueresource);
+
+Vue.http.options.root = "http://cadastro-app.herokuapp.com/";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
